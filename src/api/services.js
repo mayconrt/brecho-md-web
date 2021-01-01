2b51444b80c517a5caccedfd19ee4fb37c58e445
Maycon Rogerio Tenorio de Albuquerque <mayconrt@ciandt.com>
2021-01-01 14:53:28 -0300

    config heroku deploy

--- src/api/services.js
+++ src/api/services.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://brecho-md-api.herokuapp.com'
 
 export const URL_AUTH = '/auth'
 export const URL_PRODUCT = '/product'
